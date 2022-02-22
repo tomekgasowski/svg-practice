be719728fce01dd98ab5a58670b81f0b5aefc045
Tomek Gasowski <tomekgasowski93@gmail.com>
2022-02-22 21:32:29 +0100

    js localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -10,13 +10,23 @@ const counter = document.querySelector('.glass__counter--js');
 const addButton = document.querySelector('.button__add--js');
 const removeButton = document.querySelector('.button__remove--js');
 
-let glassNumber = ''
+const key = new Date().toISOString().slice(0,10);
+
+if (!(localStorage.getItem(key))) {
+    counter.innerHTML = 0;
+    localStorage.setItem(key, 0);
+} else {
+    counter.innerHTML = localStorage.getItem(key);
+}
+
+let glassNumber = localStorage.getItem(key);
 
 addButton.addEventListener('click', () => {
     if (glassNumber < 99) {
         glassNumber++
         counter.innerHTML = glassNumber;
     }
+    localStorage.setItem(key, glassNumber);
 });
 
 removeButton.addEventListener('click', () => {
@@ -24,4 +34,28 @@ removeButton.addEventListener('click', () => {
         glassNumber--;
         counter.innerHTML = glassNumber;
     }
+    localStorage.setItem(key, glassNumber);
 })
+
+// const lenght = localStorage.length;
+
+// for (let i = 0; i < lenght; i++) {
+//     let keyName = window.localStorage.key(i);
+//     if (keyName.length < 11) {
+//     console.log(keyName)
+//     }
+// }
+
+// console.log(Date.parse(window.localStorage.key(2)));
+// console.log(Date.parse(window.localStorage.key(3)));
+
+
+// let dt = new Date();
+// let month = dt. getMonth();
+// let year = dt. getFullYear();
+// let daysInMonth = new Date(year, month, 0). getDate();
+
+// console.log(dt);
+// console.log(month);
+// console.log(year);
+// console.log(daysInMonth);
